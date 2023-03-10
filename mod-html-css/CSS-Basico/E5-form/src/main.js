c9bd024ea2f16ff1033201d5d1657f1a04a32cb9
bportela <bportela.dev@gmail.com>
2023-03-10 18:37:43 +0100

    Comentarios del JS para el profesor

--- mod-html-css/CSS-Basico/E5-form/src/main.js
+++ mod-html-css/CSS-Basico/E5-form/src/main.js
@@ -1,35 +1,58 @@
+/**
+ * REST COUNTRIES https://restcountries.com/ es una RESTFull API para obtener información sobre países.
+ * 
+ * El endpoint permite obtener los paises de europa, incluyendo sólo los campos name y translations, que
+ * representa el nombre del país y los nombres traducidos a diferentes idiomas. Hay muchos otros campos
+ * que se pueden obtener consultando la documentación.
+ */
 const URL_COUNTRIES_EUROPE = 'https://restcountries.com/v3.1/region/europe?fields=name,translations';
 
-function fetchCountries() {
-    fetch(URL_COUNTRIES_EUROPE, { method: 'GET', mode: 'cors' })
+/**
+ * Obtiene la lista de paises europeos.
+ * @returns {Promise<string[]>} Promise que se resuelve con la lista de nombres de países
+ */
+async function fetchCountries() {
+    return fetch(URL_COUNTRIES_EUROPE, { method: 'GET', mode: 'cors' })
         .then(response => {
             if (response.ok)
                 return response.json();
             else
                 return null;
         })
-        .catch(r => null)
         .then(response => {
-            let countries;
-            if (response === null)
-                countries = ['Inglaterra', 'España', 'Irlanda', 'Francia'];
-            else
-                countries = response
-                    .map(obj => obj.translations.spa.common)
-                    .sort((a, b) => a > b);
-            fillSelector(countries);
+            const countries = response
+                .map(obj => obj.translations.spa.common)
+                .sort((a, b) => a > b);
+            return countries;
         })
 }
-function fillSelector(countries) {
+
+/**
+ * Rellena el selector de países con la lista de nombres que se recibe
+ * @param {string[]} countries Array con países
+ */
+function fillSelector(countries = []) {
+    if (countries.count == 0)
+        return;
+
     const selector = document.getElementById('country-selector');
-    selector.innerHTML = '';
+    selector.innerHTML = ''; // Quita los que ya existen
     for (const country of countries) {
         const opt = document.createElement('option');
         opt.innerHTML = country;
+        // Preestablece como seleccionado el de españa, si existe.
         if (country == 'España')
             opt.setAttribute('selected', true);
         selector.appendChild(opt);
     }
 }
 
-window.onload = fetchCountries();
+window.onload = () => {
+    fetchCountries()
+        .then(countries => fillSelector(countries))
+        .catch(err => {
+            console.error('No se consigieron los paises. Se escriben unos por defecto. Error original:', err);
+            let countries = ['Inglaterra', 'España', 'Irlanda', 'Francia'];
+            fillSelector(countries);
+        })
+};
