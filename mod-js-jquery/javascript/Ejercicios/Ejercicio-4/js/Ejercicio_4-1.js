914717a6f73f604a780289032959a6e78a698ddb
bportelalp <bportelalopez@outlook.es>
2023-04-18 19:15:03 +0200

    Ejercicio 4.1 Escribir fecha

--- mod-js-jquery/javascript/Ejercicios/Ejercicio-4/js/Ejercicio_4-1.js
+++ mod-js-jquery/javascript/Ejercicios/Ejercicio-4/js/Ejercicio_4-1.js
@@ -1,19 +1,28 @@
 // JavaScript Document
+const days = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 
-
-// La función escribe() tiene un parámetro de entrada con una cadena de texto con el código
-// HTML que se quiere mostrar dentro de la capa con ID "cuadro_texto". Esta función el alumno
-// tiene que usarla pero no es necesario modificarla
+/**
+ * Escribe el texto en el elemento "cuadro_texto"
+ * @param {string} texto 
+ */
 function escribe(texto) {
-
 	document.getElementById("cuadro_texto").innerHTML = texto;
-
 }
 
+/**
+ * Escribe la fecha actual en el documento HTML
+ */
 function ponfecha() {
-	/**A diferencia de la struct DateTime de C#, en JS es mas limitado el formateo y no tiene una compatibilidad
+	/**
+	 * A diferencia de la struct DateTime de C#, en JS es mas limitado el formateo y no tiene una compatibilidad
 	 * como con System.Globalization para construir una fecha completa. En esta función se hace sin usar librerías de npm
 	 */
 	const now = new Date();
-	escribe(now.tosDateString());
+	const dayOfWeek = now.getDay();
+	const day = now.getDate();
+	const month = now.getMonth();
+	const year = now.getFullYear();
+	const dateFormatted = `${days[dayOfWeek]}, ${day} de ${months[month]} de ${year}`;
+	escribe('Hoy es <b>' + dateFormatted + '</b>');
 }
