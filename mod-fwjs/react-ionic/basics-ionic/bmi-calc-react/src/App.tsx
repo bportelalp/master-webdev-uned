c2f6943b96ba93748c8a9db5cac3e8d7914c9130
bportelalp <bportelalopez@outlook.es>
2023-06-23 13:04:12 +0200

    Calculadora IMC con Ionic y React

--- mod-fwjs/react-ionic/basics-ionic/bmi-calc-react/src/App.tsx
+++ mod-fwjs/react-ionic/basics-ionic/bmi-calc-react/src/App.tsx
@@ -1,7 +1,4 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import { IonApp, IonContent, IonHeader, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,21 +18,22 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import BmcForm from './components/BmiForm';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>
+          Calculadora de IMC con componentes ionic
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className='ion-padding'>
+      <BmcForm/>
+    </IonContent>
   </IonApp>
 );
 
