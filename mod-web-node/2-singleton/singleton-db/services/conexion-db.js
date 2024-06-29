d0c5674e2aa5d0eaaae7e1966d1e8cdc1427a6fe
bportelalp <bportelalopez@outlook.es>
2024-06-29 17:35:53 +0200

    Escucha de eventos conexion

--- mod-web-node/2-singleton/singleton-db/services/conexion-db.js
+++ mod-web-node/2-singleton/singleton-db/services/conexion-db.js
@@ -4,6 +4,10 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGO_DB;
 const client = new MongoClient(uri, { compressors: "zlib" });
 
+client.on("connectionPoolCreated", event => debug("connectionPoolCreated", event));
+client.on("connectionPoolClosed", event => debug("connectionPoolClosed", event));
+
+
 const loadDb = async function run() {
   try {
     await client.connect();
