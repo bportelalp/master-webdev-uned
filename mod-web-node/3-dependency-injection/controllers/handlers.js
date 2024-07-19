9e3b87925f752d5024ca81093889672b0106ba04
bportelalp <bportelalopez@outlook.es>
2024-07-19 20:15:37 +0200

    Hacer implementación con MongoDb

--- mod-web-node/3-dependency-injection/controllers/handlers.js
+++ mod-web-node/3-dependency-injection/controllers/handlers.js
@@ -1,24 +1,22 @@
-module.exports = resource => ({instanciaJsonPlaceHolder}) => ({
-    get: async (req, res) =>
-        {
-           const { data} = await instanciaJsonPlaceHolder.get(`/${resource}`) 
-           return res.send(data)
-        },
-    getId: async (req, res) =>
-        {
-            const { data} = await instanciaJsonPlaceHolder.get(`/${resource}/${req.params.id}`) 
-            return res.send(data)
-        },        
+module.exports = resource => ({ instanciaJsonPlaceHolder: jsonPlaceholderClient }) => ({
+    get: async (req, res) => {
+        const { data } = await jsonPlaceholderClient.get(resource)
+        return res.send(data)
+    },
+    getId: async (req, res) => {
+        const { data } = await jsonPlaceholderClient.getById(resource, req.params.id)
+        return res.send(data)
+    },
     post: async (req, res) => {
-        const { data} = await instanciaJsonPlaceHolder.post(`/${resource}`, req.body) 
+        const { data } = await jsonPlaceholderClient.post(resource, req.body)
         return res.send(data)
     },
     put: async (req, res) => {
-        const { data} = await instanciaJsonPlaceHolder.put(`/${resource}/${req.params.id}`, req.body) 
+        const { data } = await jsonPlaceholderClient.put(resource, req.params.id, req.body)
         return res.send(data)
     },
     delete: async (req, res) => {
-        const { data} = await instanciaJsonPlaceHolder.delete(`/${resource}/${req.params.id}`) 
+        const { data } = await jsonPlaceholderClient.delete(resource, req.params.id,)
         return res.sendStatus(204)
     }
 })
--- mod-web-node/3-dependency-injection/services/index.js
+++ mod-web-node/3-dependency-injection/services/index.js
@@ -1,7 +1,13 @@
-const {instanciaJsonPlaceHolder} = require('./axios');
-const cliente = require('./conexion-db');
+const {instanciaJsonPlaceHolder: instanciaAPI} = require('./jsonPlaceholderApi');
+const {instanciaJsonPlaceHolder: instanciaDb} = require('./jsonPlaceholderDb');
+require('dotenv').config();
+
+let implementation;
+if(process.env.JSON_PLACEHOLDER_CLIENT == 'API')
+    implementation = instanciaAPI;
+else if (process.env.JSON_PLACEHOLDER_CLIENT == 'DB')
+    implementation = instanciaDb;
 
 module.exports = {
-    instanciaJsonPlaceHolder: instanciaJsonPlaceHolder,
-    cliente: cliente
+    instanciaJsonPlaceHolder: implementation
 }
