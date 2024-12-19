92abe8de5d52bb2b6e7494c0e856a452b5756dfb
bportelalp <bportelalopez@outlook.es>
2024-12-19 20:41:03 +0100

    Endpoint post y get resultados de partidas

--- 8-mod-tfm/tabletop-api/routes/game-results.js
+++ 8-mod-tfm/tabletop-api/routes/game-results.js
@@ -1,15 +1,44 @@
 import express from 'express'
 import debugLib from 'debug';
 import repo from '../services/game-results-repo.js'
-
-
-const debug = debugLib("api:games")
+import validator from '../middlewares/body-validator.js';
+import { ApiError } from '../helpers/ApiError.js';
+import { validationResult, query, param } from 'express-validator';
 
 var router = express.Router();
 
-router.post('', (req, res, next) => {
+const validatorQueryGetResults = [
+    param('id').notEmpty().withMessage('Debes enviar un id como parámetro'),
+    param('id').isString().isLength({ min: 24, max: 24 }).withMessage('id debe ser un ID de 24 caracteres'),
+    query('fromDate').notEmpty().withMessage('fromDate es obligatorio en la query'),
+    query('fromDate').isISO8601().withMessage('fromDate debe ser una fecha válida en formato ISO 8601'),
+    query('toDate').notEmpty().withMessage('toDate es obligatorio en la query'),
+    query('toDate').isISO8601().withMessage('toDate debe ser una fecha válida en formato ISO 8601'),
+]
+
+router.get('/:id', validatorQueryGetResults, async (req, res, next) => {
     try {
-        
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            throw new ApiError(400, "Hay errores de validación en los datos", { errors: errors.array() });
+
+        const result = await repo.getResults(req.params.id, req.query.fromDate, req.query.toDate);
+        res.send(result);
+
+    } catch (error) {
+        next(error);
+    }
+})
+
+router.post('', validator.validateGameResult(), (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty())
+            throw new ApiError(400, "Hay errores de validación en los datos", { errors: errors.array() });
+
+        repo.addResult(req.body);
+        res.status(201).send();
+
     } catch (error) {
         next(error);
     }
--- 8-mod-tfm/tabletop-api/services/game-results-repo.js
+++ 8-mod-tfm/tabletop-api/services/game-results-repo.js
@@ -5,25 +5,41 @@ import { ApiError } from '../helpers/ApiError.js'
 const debug = debugLib("api:repo");
 
 async function addResult(data) {
-    _checkGameExist(data.gameId);
+    await _checkGameExist(data.gameId);
+    data.playedOn = new Date(data.playedOn); // Fuerza formato fecha
     await db.collection('game_results').insertOne(data);
 }
 
+async function getResults(gameId, from, to) {
+    await _checkGameExist(gameId);
+    const query = {
+        gameId: gameId,
+        playedOn: {
+            $gte: new Date(from),
+            $lte: new Date(to)
+        }
+    };
+    const results = await db.collection('game_results').find(query).toArray();
+    return results;
+}
 
 async function _checkGameExist(id) {
     const game = await db.collection('games').findOne({
         _id: _validateAndGetId(id)
     })
-    if (!game)
-        throw new ApiError(400, "No se puede insertar resultados de un juego que no existe");
+    if (!game) {
+        throw new ApiError(400, "El juego no existe");
+    }
 }
 
 function _validateAndGetId(id) {
     if (!ObjectId.isValid(id)) {
+        debug('Intento de buscar juego con id inválido:', id);
         throw new ApiError(400, "El formato de id no es válido");
     }
     return ObjectId.createFromHexString(id);
 }
 export default {
-    addResult
+    addResult,
+    getResults
 }
