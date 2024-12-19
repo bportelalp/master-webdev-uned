38c37ba7ce9baf95b09c22a24260d602b5ac2aa1
bportelalp <bportelalopez@outlook.es>
2024-12-19 18:51:29 +0100

    Mejoras comentarios del cliente

--- 8-mod-tfm/tabletop-webapp/src/services/tabletop-client.js
+++ 8-mod-tfm/tabletop-webapp/src/services/tabletop-client.js
@@ -3,78 +3,113 @@ import axios from 'axios';
 
 const URI = process.env.REACT_APP_TABLETOP_API;
 
-
-const getGames = () => {
+/**
+ * Obtener lista de juegos completa
+ * @returns Array con juegos
+ */
+const getGames = async () => {
     const uri = `${URI}/api/games`;
     try {
-        return axios.get(uri)
-            .then(resp => resp.data);
+        const resp = await axios.get(uri);
+        return resp.data;
     } catch (error) {
         console.log('Error on', uri, error);
         throw new Error(error.message);
     }
 }
 
-const getGame = (gameId) => {
+/**
+ * Busca un juego por ID.
+ * @param {string} gameId Id de juego a buscar
+ * @returns Juego encontrado o null si no existe
+ */
+const getGame = async (gameId) => {
     const uri = `${URI}/api/games/${gameId}`;
 
     try {
-        const promise = axios.get(uri)
-            .then(resp => resp.data);
-        return promise;
+        const resp = await axios.get(uri);
+        return resp.data;
     } catch (error) {
+        // Si el error es un 404, devolver null, porque el juego no existe
+        if (error.response && error.response.status === 404) {
+            return null;
+        }
+
         console.log('Error on', uri, error);
         throw new Error(error.message);
     }
 }
 
-const createGame = (game) => {
+
+/**
+ * Crea un juego nuevo
+ * @param {*} game objeto de datos
+ * @returns El objeto creado, con el nuevo id generado.
+ */
+const createGame = async (game) => {
     const uri = `${URI}/api/games`;
-    if (game._id)
-        delete game._id;
+    if (game._id) delete game._id;
+    
     try {
-        const promise = axios.post(uri, game)
-            .then(resp => resp.data);
-        return promise;
+        const resp = await axios.post(uri, game);
+        return resp.data;
     } catch (error) {
         console.log('Error on', uri, error);
         throw new Error(error.message);
     }
 }
 
-const updateGame = (gameId, game) => {
+
+/**
+ * Edita un juego existente.
+ * @param {string} gameId id del objeto a manipular
+ * @param {*} game objeto de datos
+ * @returns El objeto actualizado por completo.
+ */
+const updateGame = async (gameId, game) => {
     const uri = `${URI}/api/games/${gameId}`;
-    if (game._id)
-        delete game._id;
+    if (game._id) delete game._id;
 
     try {
-        const promise = axios.put(uri, game)
-            .then(resp => resp.data);
-        return promise;
+        const resp = await axios.put(uri, game);
+        return resp.data;
     } catch (error) {
         console.log('Error on', uri, error);
         throw new Error(error.message);
     }
 }
 
-const updateGameImage = (gameId, fileImage) => {
+/**
+ * Actualiza la imagen de un juego
+ * @param {*} gameId Id del juego
+ * @param {*} fileImage archivo de imagen a enviar
+ * @returns El juego entero, con la imagen actualizada.
+ */
+const updateGameImage = async (gameId, fileImage) => {
     const uri = `${URI}/api/games/${gameId}/image`;
     const formData = new FormData();
     formData.append('image', fileImage);
+
     try {
-        const promise = axios.put(uri, formData, {
+        const resp = await axios.put(uri, formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
-        })
-            .then(resp => resp.data);
-        return promise;
+        });
+        return resp.data;
     } catch (error) {
         console.log('Error on', uri, error);
         throw error;
     }
 }
 
+
+/**
+ * Función ayuda para obtener la ruta absoluta de una imagen de un juego, puesto
+ * que la api devuelve rutas relativas.
+ * @param {*} relativePath 
+ * @returns 
+ */
 const getImgUri = (relativePath = "") => `${URI}${relativePath}`;
 
 const instance = {
